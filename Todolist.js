d4b642a83561ecb04a33f9ebaf6b28c0f977c1f8
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2023-03-14 20:35:01 +0900

    feat: todolist 를 받을 View 생성

--- Todolist.js
+++ Todolist.js
@@ -1,1 +1,10 @@
+const root = document.getElementById("root");
+const Form = document.createElement("form");
+const Input = document.createElement("input");
+const Button = document.createElement("button");
+root.appendChild(Form);
+Form.appendChild(Input);
+Form.appendChild(Button);
 
+Input.placeholder = "Todolist";
+Button.innerText = "제출!!!";
