a8cb497199d2bd5d50bc3f64124a7d526b502034
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2023-03-14 20:46:34 +0900

    refactor: 배열로 todolist를 관리해서 todolist를 기억하도록함

--- Todolist.js
+++ Todolist.js
@@ -2,20 +2,23 @@ const root = document.getElementById("root");
 const Form = document.createElement("form");
 const Input = document.createElement("input");
 const Button = document.createElement("button");
+const Ul = document.createElement("ul");
 root.appendChild(Form);
+root.appendChild(Ul);
 Form.appendChild(Input);
 Form.appendChild(Button);
 
 Input.placeholder = "Todolist";
 Button.innerText = "제출!!!";
 
-let todolist;
+let todolist = [];
 
 Form.addEventListener("submit", (event) => {
   event.preventDefault();
-  todolist = event.target[0].value;
+  todolist.push(event.target[0].value);
   event.target[0].value = "";
   Paint();
+  Remove();
 });
 
 //데이터담기
@@ -23,7 +26,8 @@ Form.addEventListener("submit", (event) => {
 //이제 데이터 그리기
 
 function Paint() {
-  const Ul = document.createElement("ul");
-  root.appendChild(Ul);
-  Ul.innerHTML = `<li>${todolist}<button>삭젱</button></li>`;
+  const Ul = document.querySelector("ul");
+  Ul.innerHTML = `${todolist
+    .map((item) => `<li>${item}<button class="REMOVE">삭젱</button></li>`)
+    .join(``)}`;
 }
